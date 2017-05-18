817b64222951cfa3fcae38940a59c8b326f9c61e
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-18 15:18:16 +0200

    test controlleur

--- test.php
+++ test.php
@@ -18,18 +18,15 @@
 
 <body>
 
-  <form methode=post action="accueil.controller.php">
 
-<select name="region" id="region">
-                  <?php
-                      foreach ($regions as $region){
-                        echo '<option value="'.$region['id'].'">'.$region['nomregion'].'</option>'; //Affiche chaque nom (ex: Informatique et Gestion) de chaque département de la base de données
-                      }
-                  ?>
-</select>
+            <select name="region" id="region">
+                              <?php
+                                  foreach ($regions as $region){
+                                    echo '<option value="'.$region['id'].'">'.$region['nomregion'].'</option>'; //Affiche chaque nom (ex: Informatique et Gestion) de chaque département de la base de données
+                                  }
+                              ?>
+            </select>
 
-
-</form>
 
 </body>
 
