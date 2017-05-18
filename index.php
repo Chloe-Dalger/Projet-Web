065a4ab221420734b221ec81ae2fc00e18b9b129
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-18 16:39:29 +0200

    test controlleur

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8"/>
-		<title>Lieu</title>
-		<link rel="stylesheet" href="style.css" />
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<meta charset="UTF-8"/>
 	</head>
 	<body>
 		<?php
