c639f5ca16667dbf449110e7ce7bb79346e8c182
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-18 16:29:02 +0200

    test controlleur

--- controller/ajoutlieu_controller.php
+++ controller/ajoutlieu_controller.php
@@ -1,7 +1,5 @@
 <?php
-  require_once('../model/DBconnect.php');
-  require_once('../model/region.php');
-  require_once('../model/departement.php');
+
   require_once('view/ajoutlieu.php');
 
   ?>
