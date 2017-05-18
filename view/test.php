92f7d269c416ac7f33b634f2b2671d43cfe40ab8
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-18 17:14:04 +0200

    test controlleur

--- view/test.php
+++ view/test.php
@@ -17,25 +17,64 @@
 </head>
 
 <body>
+  <div class="body-search">
+        <div class="site-wrapper">
+            <div class="site-wrapper-inner">
+                  <div class="cover-container">
+
+                        <h1 class="cover-heading">Trouve l'endroit de tes rêves</h1>
+                        <p class="lead">Rentre la ville, choisi ta région et ton département et découvre les endroits à visiter!</p>
+                        <form methode=post action="accueil.controller.php">
+                            <input list="browsers">
+                                  <div class="input-group">
+
+                                          <div class="input-group-btn search-panel">
+
+                                                <ul class="dropdown-menu" role="menu">
 
 
-            <select name="region" id="region">
-                              <?php
-                                  foreach ($regions as $region){
-                                    echo '<option value="'.$region['idregion'].'">'.$region['nomregion'].'</option>'; //Affiche chaque nom (ex: Informatique et Gestion) de chaque département de la base de données
-                                  }
-                              ?>
-            </select>
-
-            <select name="depart" id="depart">
-                              <?php
-                                  foreach ($departs as $depart){
-                                    echo '<option value="'.$depart['iddep'].'">'.$depart['nomdep'].'('.$depart['numerodep'].')</option>'; //Affiche chaque nom (ex: Informatique et Gestion) de chaque département de la base de données
-                                  }
-                              ?>
-            </select>
+                                                              <select name="region" id="region">
+                                                                                <?php
+                                                                                    foreach ($regions as $region){
+                                                                                      echo '<option value="'.$region['idregion'].'">'.$region['nomregion'].'</option>'; //Affiche chaque nom (ex: Informatique et Gestion) de chaque département de la base de données
+                                                                                    }
+                                                                                ?>
+                                                              </select>
 
 
-</body>
+                                                  </ul>
+                                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                                                <span id="search_concept">Région</span> <span class="caret"></span>
+                                                            </button>
+                                                            <ul class="dropdown-menu" role="menu">
+                                                              <select name="depart" id="depart">
+                                                                                <?php
+                                                                                    foreach ($departs as $depart){
+                                                                                      echo '<option value="'.$depart['iddep'].'">'.$depart['nomdep'].' ('.$depart['numerodep'].')</option>'; //Affiche chaque nom (ex: Informatique et Gestion) de chaque département de la base de données
+                                                                                    }
+                                                                                ?>
+                                                              </select>
+                                                            </ul>
+                                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                                                <span id="search_concept">Département</span> <span class="caret"></span>
+                                                            </button>
+                                                      </div>
+                                                      <input type="hidden" name="search_param" value="all" id="search_param">
+                                                      <input type="text" class="form-control" name="x" placeholder="Entrer ville...">
+                                                      <span class="input-group-btn">
+                                                          <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+                                                      </span>
+                                                    </div>
+                                      </form>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+
+              </body>
+
+            </html>
+
+
 
 </html>
