376267c20b1a86d885ebcc8c1275eac72d4a65ee
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-18 15:13:54 +0200

    test controlleur

--- test.php
+++ test.php
@@ -14,13 +14,6 @@
 
 <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-<style>
-body {
-color:black;
-background-color:white;
-background-image:url(http://mag.monchval.com/wp-content/uploads/2015/12/choisir-la-race-de-son-lapin.png);
-}
-</style>
 </head>
 
 <body>
