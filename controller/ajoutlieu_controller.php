1fb7d1b4d631d7abd39642d66ce2ae957028569a
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-19 17:43:16 +0200

    ajout lieu

--- controller/ajoutlieu_controller.php
+++ controller/ajoutlieu_controller.php
@@ -3,6 +3,10 @@
   require_once('../model/ville.php');
     require_once('../model/departement.php');
 
+      require_once('../model/pseudo.php');
+      require_once('../model/lieu.php');
+      require_once('../model/categorie.php');
+      require_once('../model/mot_cle.php');
 
 
 
