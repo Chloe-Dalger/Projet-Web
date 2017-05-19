a3a68f59e2f5b55a0b9042988626f82323ae52ad
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-19 20:31:17 +0200

    ajout lieu*

--- controller/ajoutlieu_controller.php
+++ controller/ajoutlieu_controller.php
@@ -48,9 +48,51 @@ $categories=getAllCategorie();
       else
     {
 
+      if(!is_null(existeLieu($nomlieu, $ville))){
 
 
-    }
+        if(is_null(existeVille($cpville))){
+          $dep=substr($cpville, 0, 1);
+          if(is_null(existeDepartement($dep))){
+            $message = 'Le code postal n''est pas valide';
+            $bool = false;
+          }else {
+            $iddep=getIdNDepartement($dep);
+            creerVille($ville,$cpville,$iddep);
+          }
+        }
+
+        if($bool){
+              if(is_null(existePseudo($pseudo))){
+                creerPseudo($pseudo);
+              }
+              $idpseudo=getIdPseudo($pseudo);
+              $idville=getIdVille($ville);
+              $idcat=getIdCategorie($categorie);
+              creerLieu($nomlieu, $url, $description, $adr, $idpseudo, $idville, $idcat);
+              $idlieu=getIdLieu($nom);
+              for($i=0; $i<count($motcle); $i++){
+                if(is_null($motcle[$i])){
+                  creerMotCle($motcle[$i]);
+                  $idmotcle=getIdMotCle($motcle[$i]);
+                  creerMotCleLieu($idlieu, $idmotcle);
+                }
+              }
+              // L'identification a réussi
+            $message = 'Votre lieu a bien été ajouté !';
+        }
+
+
+
+      }else{
+      $message='Le lieu dans cette ville existe déjà';
+      }
+
+
+
+      }
+
+
 
 
   include('../view/test.php');
