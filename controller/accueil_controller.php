63bb7350358260de0f1c2ff47689cd9c3b4f2773
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-19 17:52:15 +0200

    ajout lieu*

--- controller/accueil_controller.php
+++ controller/accueil_controller.php
@@ -2,6 +2,7 @@
   require_once('../model/DBconnect.php');
   require_once('../model/region.php');
   require_once('../model/departement.php');
+        require_once('../model/mot_cle.php');
 
 
   $regions=getAllRegion();
