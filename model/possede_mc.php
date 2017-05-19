800aea50566ca1defa82effdf8a86e38eb9e3574
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-19 18:24:53 +0200

    ajout lieu*

--- model/possede_mc.php
+++ model/possede_mc.php
@@ -17,20 +17,5 @@ function creerMotCle($idl, $idmc){
 
 }
 
-function supprimerMotCleLieu($id){
-  //donnée : id du mot cle à supprimer
-	//pré : idmotcle: entier >0
-	//résultat : suppression du mot cle de la base de données
-  global $db;
-  try{
-    $req=$db->prepare('DELETE FROM possede_mc WHERE idmotcle=:id OR idlieu=:id');
-		$req->execute(array($id));
-	} catch(PDOException $e){
-			echo($e->getMessage());
-			die(" Erreur lors de la suppression du mot cle dans la base de données " );
-  }
-}
-
-
 
  ?>
