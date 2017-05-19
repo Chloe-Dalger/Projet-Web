0884f942762f07e2067c2702aca0077c6216ab89
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-19 18:17:17 +0200

    ajout lieu*

--- model/possede_mc.php
+++ model/possede_mc.php
@@ -18,6 +18,23 @@ function existeMotCleLieu($idmc, $idlieu){
 
 }
 
+function creerMotCle($idl, $idmc){
+  //donnée : nom du mot clé
+  //pré : nom : String & length(nom)>0
+  //résultat : ajout du mot cle dans la base de données
+
+  global $db;
+	try{
+		$req=$db->prepare('INSERT INTO possede_mc(idlieu, idmotcle) VALUES (?, ?)');
+		$req->execute(array($idl, $idmc));
+	} catch(PDOException $e){
+		echo($e->getMessage());
+		die(" Erreur lors l'insertion de la region dans la base de données " );
+}
+
+}
+
+
 
 
  ?>
