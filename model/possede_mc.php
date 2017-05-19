6a2cbe07d75d4aea2e8ae868345313f5a2f1f2a1
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-19 18:20:55 +0200

    ajout lieu*

--- model/possede_mc.php
+++ model/possede_mc.php
@@ -1,22 +1,5 @@
 <?php
 
-function existeMotCleLieu($idmc, $idlieu){
-  //données : mot cle
-	//pré : lib: String
-	//résultat : id du mot cle s'il existe, NULL sinon
-	//post : idmotcle : entier >0 ou NULL
-  global $db;
-	try{
-		$req=$db->prepare('SELECT idmotcle FROM possede_mc WHERE idmotcle=? AND idlieu=? ');
-		$req->execute(array($idmc, $idlieu));
-		$idmotcle=$req->fetch();
-	} catch(PDOException $e){
-		echo($e->getMessage());
-		die(" Erreur lors de la vérification de l'existence du mot cle dans la table " );
-}
-		return $idmotcle[0];
-
-}
 
 function creerMotCle($idl, $idmc){
   //donnée : nom du mot clé
@@ -34,6 +17,19 @@ function creerMotCle($idl, $idmc){
 
 }
 
+function supprimerMotCleLieu($id){
+  //donnée : id du mot cle à supprimer
+	//pré : idmotcle: entier >0
+	//résultat : suppression du mot cle de la base de données
+  global $db;
+  try{
+    $req=$db->prepare('DELETE FROM possede_mc WHERE idmotcle=:id OR idlieu=:id');
+		$req->execute(array($id));
+	} catch(PDOException $e){
+			echo($e->getMessage());
+			die(" Erreur lors de la suppression du mot cle dans la base de données " );
+  }
+}
 
 
 
