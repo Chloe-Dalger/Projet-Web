5286c0180d83f9bb1354172175050b350500c78f
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-19 18:59:12 +0200

    ajout lieu*

--- model/possede_mc.php
+++ model/possede_mc.php
@@ -36,5 +36,21 @@ function supprimerMCLieu($id){
   }
 }
 
+function creerMCLieu($idm, $idl){
+	//donnée : nom du lieu, l'url de l'image correspondant au lieu, description du lieu, adresse du lieu, pseudo de celui qui a ajouté de lieu, id de la ville dans laquelle se trouve le lieu et id de la catégorie à laquelle appartient le lieu
+	//pré : nom : String & length(nom)>0, url, adr, des: String ou NULL, idpseudo, idville, idcat: Entier>0
+	//résultat : ajout du lieu dans la base de données
+
+  global $db;
+	try{
+		$req=$db->prepare('INSERT INTO possede_mc(idmotcle, idlieu) VALUES (?,?)');
+		$req->execute(array($idm, $idl));
+	} catch(PDOException $e){
+		echo($e->getMessage());
+		die(" Erreur lors l'insertion du lieu dans la base de données " );
+}
+
+}
+
 
  ?>
