519f32806618522e563900f4ae6e77c04489c73a
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-20 18:19:36 +0200

    affichage lieu

--- model/lieu.php
+++ model/lieu.php
@@ -382,8 +382,8 @@ function getAllVilleDepartementRegionLieu($idville, $iddepartement, $idregion){
 
 						global $db;
 						try{
-								$req=$db->query('SELECT nomlieu, urllieu, deslieu, adrlieu FROM lieu WHERE lieu.idville IN (SELECT ville.idville FROM ville WHERE ville.iddep IN (SELECT departement.iddep FROM departement WHERE departement.idregion=?))');
-								$req->execute(array());
+								$req=$db->prepare('SELECT nomlieu, urllieu, deslieu, adrlieu FROM lieu WHERE lieu.idville IN (SELECT ville.idville FROM ville WHERE ville.iddep IN (SELECT departement.iddep FROM departement WHERE departement.idregion=?))');
+								$req->execute(array($idregion));
 								$Listelieu=$req->fetchAll();
 							} catch(PDOException $e){
 									echo($e->getMessage());
