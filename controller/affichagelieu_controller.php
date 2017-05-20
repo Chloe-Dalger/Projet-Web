f9697a3165ca818d9bb37c12fde8a42c7d95bd99
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-20 16:59:07 +0200

    affichage lieu

--- controller/affichagelieu_controller.php
+++ controller/affichagelieu_controller.php
@@ -1,6 +1,15 @@
     <?php
+    require_once('../model/DBconnect.php');
+    require_once('../model/ville.php');
+    require_once('../model/departement.php');
+    require_once('../model/pseudo.php');
+    require_once('../model/lieu.php');
+    require_once('../model/categorie.php');
+    require_once('../model/mot_cle.php');
+    require_once('../model/possede_mc.php');
+    require_once('../model/region.php');
+    require_once('../model/essaie.php');
 
-      require_once('../model/lieu.php');
 
 
 
