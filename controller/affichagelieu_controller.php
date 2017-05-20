091acc29e48c34ff15e08ec8cb770dee5878913b
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-20 16:56:29 +0200

    affichage lieu

--- controller/affichagelieu_controller.php
+++ controller/affichagelieu_controller.php
@@ -1,22 +1,4 @@
     <?php
-      require_once('../model/DBconnect.php');
-      require_once('../model/ville.php');
-      require_once('../model/departement.php');
-      require_once('../model/pseudo.php');
-      require_once('../model/lieu.php');
-      require_once('../model/categorie.php');
-      require_once('../model/mot_cle.php');
-      require_once('../model/possede_mc.php');
-      require_once('../model/region.php');
-      require_once('../model/essaie.php');
-
-          $ville=$_POST['ville'];
-          $region=$_POST['region'];
-          $depart=$_POST['depart'];
-
-          $iddep=getIdDepartement($depart);
-          $idregion=getIdRegion($region);
-
 
 
             include('../view/essaie.php');
