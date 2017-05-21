e916b38ce317ff90b0fd5d1c54d7bf7cc77d92ed
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-21 18:02:25 +0200

    affichage lieu

--- view/affichagelieu.php
+++ view/affichagelieu.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
 <link rel="stylesheet" href="../view/css/navbar.css">
 <style>
 h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif;
