1918218eb9710ec7fd046b828a99f597421ef29e
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-21 17:55:08 +0200

    affichage lieu

--- view/affichagelieu.php
+++ view/affichagelieu.php
@@ -9,8 +9,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="../bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
 <link rel="stylesheet" href="../view/css/navbar.css">
 
