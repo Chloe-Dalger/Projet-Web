85a9f6e1b54cb816f82e6d91b1c5309d3395e548
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-21 15:13:54 +0200

    affichage lieu

--- view/affichagelieu.php
+++ view/affichagelieu.php
@@ -58,7 +58,7 @@ header{
     <?php
     foreach ($lieux as $lieu){ ?>
     <div class="w3-third w3-container w3-margin-bottom">
-      <?php echo '<a href="../controller/ajoutdulieu_controller.php?nom='.$lieu['nomlieu'].'">';?>
+      <?php echo '<a href="../controller/affichagedulieu_controller.php?nom='.$lieu['nomlieu'].'">';?>
       <?php
 
         echo '<img src="'.$lieu['urllieu'].'" style="width:100%; height: 100%;"" class="w3-hover-opacity">';
