c3342d4edd4bfdb19d9a4006591c64026fd2d5c6
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-21 13:30:29 +0200

    affichage lieu

--- view/essaie.php
+++ view/essaie.php
@@ -62,7 +62,7 @@ header{
     <div class="w3-third w3-container w3-margin-bottom">
       <?php
 
-        echo '<img src="'.$lieu['urllieu'].'" style="width:100%; height: auto;"" class="w3-hover-opacity">';
+        echo '<img src="'.$lieu['urllieu'].'" style="width:100%; height: 100%;"" class="w3-hover-opacity">';
       ?> <div class="w3-container w3-white">
         <p><b><?php echo $lieu['nomlieu']; ?></b></p>
         <p><?php echo $lieu['deslieu']; ?></p>
