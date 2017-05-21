591d86c5069c362e94882b2fa71609cd1cebdda5
Chloe-Dalger <chloe.dalger@etu.umontpellier.fr>
2017-05-21 18:00:41 +0200

    affichage lieu

--- view/affichagelieu.php
+++ view/affichagelieu.php
@@ -7,11 +7,16 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="../view/css/navbar.css">
 <style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif;
+h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif;
 background-image: url("http://subtlepatterns.com/patterns/wood_pattern.png");
 background-repeat: repeat;}
+body{background-image: url("http://subtlepatterns.com/patterns/wood_pattern.png");
+background-repeat: repeat;}
+
+}
 header{
   margin-bottom: 30px;
 }
@@ -64,7 +69,7 @@ header{
     <h1><b> Résultats de votre recherche:</b></h1>
 
     <div class="w3-section w3-bottombar w3-padding-16">
-      <span class="w3-margin-right w3-white">Filtres:</span>
+      <span class="w3-margin-right">Filtres:</span>
 
       <?php if(!empty($ville)) : ?>
           <button class="w3-button w3-black"><?php echo $ville; ?></button>
